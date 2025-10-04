6d8ef1e990cd99b3db131e6d33e5aee9372b118a
suffff21 <yusufstwn24@gmail.com>
2025-10-04 16:10:36 +0700

    Fix: remove nested mlflow.start_run and lock sklearn version

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -19,20 +19,18 @@ X_test = pd.read_csv(f"{args.dataset_path}/X_test.csv")
 y_train = pd.read_csv(f"{args.dataset_path}/y_train.csv").squeeze()
 y_test = pd.read_csv(f"{args.dataset_path}/y_test.csv").squeeze()
 
-# Setup MLflow lokal
 mlflow.set_tracking_uri("file:./mlruns")
 mlflow.set_experiment("Spotify Churn Workflow CI")
 
 mlflow.sklearn.autolog()
 
-with mlflow.start_run(run_name="RF_CI_Basic"):
-    model = RandomForestClassifier(
-        n_estimators=args.n_estimators,
-        max_depth=args.max_depth,
-        random_state=42
-    )
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
+model = RandomForestClassifier(
+    n_estimators=args.n_estimators,
+    max_depth=args.max_depth,
+    random_state=42
+)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
 
-    acc = accuracy_score(y_test, y_pred)
-    print(f"Model selesai dilatih. Akurasi: {acc:.4f}")
+acc = accuracy_score(y_test, y_pred)
+print(f"Model selesai dilatih. Akurasi: {acc:.4f}")
